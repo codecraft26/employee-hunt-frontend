08e172230f7c7c421869c991a6e813b70d8e354c
gdivyank <gdivyankgupta@gmail.com>
2025-06-26 08:12:35 +0530

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -105,3 +105,24 @@ self.addEventListener('push', function(event) {
     self.registration.showNotification(title, notificationOptions)
   );
 });
+
+// Handle notification clicks
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  const urlToOpen = 'https://www.banndhann.com/dashboard/activities';
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window' })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url === urlToOpen && 'focus' in client) {
+            return client.focus();
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(urlToOpen);
+        }
+      })
+  );
+});
